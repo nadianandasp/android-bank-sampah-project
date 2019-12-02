a8aeadbceada2aaac02efdcd6031f047e857bcb2
Nadia Setinanda Pratami <nadiasetinandapratami@gmail.com>
2019-12-02 16:52:21 +0700

    Fixing toolbar Kode Verifikasi and finished UI Ganti Password
    -change toolbar
    -change color button

--- app/src/main/java/com/sahitya/banksampahsahitya/VerificationCodeActivity.java
+++ app/src/main/java/com/sahitya/banksampahsahitya/VerificationCodeActivity.java
@@ -1,14 +1,19 @@
 package com.sahitya.banksampahsahitya;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
 import com.google.android.material.textfield.TextInputLayout;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.Unbinder;
@@ -20,6 +25,8 @@ public class VerificationCodeActivity extends AppCompatActivity {
 
     @BindView(R.id.edit_text_verification_code)
     TextInputLayout edtVerificationCode;
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
 
     private Unbinder unbinder;
 
@@ -30,6 +37,14 @@ public class VerificationCodeActivity extends AppCompatActivity {
 
         unbinder = ButterKnife.bind(this);
 
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(null);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        final Drawable upArrow = ContextCompat.getDrawable(this, R.drawable.abc_ic_ab_back_material);
+        upArrow.setColorFilter(ContextCompat.getColor(this, R.color.black), PorterDuff.Mode.SRC_ATOP);
+        getSupportActionBar().setHomeAsUpIndicator(upArrow);
+
         btnVerificationCode.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -39,4 +54,13 @@ public class VerificationCodeActivity extends AppCompatActivity {
             }
         });
     }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == android.R.id.home){
+            onBackPressed();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
--- app/src/main/java/com/sahitya/banksampahsahitya/presentation/membership/changepassword/ChangePasswordActivity.java
+++ app/src/main/java/com/sahitya/banksampahsahitya/presentation/membership/changepassword/ChangePasswordActivity.java
@@ -1,15 +1,30 @@
 package com.sahitya.banksampahsahitya.presentation.membership.changepassword;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.sahitya.banksampahsahitya.R;
-import com.sahitya.banksampahsahitya.presentation.membership.editprofile.EditProfileActivity;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class ChangePasswordActivity extends AppCompatActivity {
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+    @BindView(R.id.btn_save)
+    Button btnSave;
 
     public static void start(Context context) {
         Intent starter = new Intent(context, ChangePasswordActivity.class);
@@ -20,5 +35,33 @@ public class ChangePasswordActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_change_password);
+        ButterKnife.bind(this);
+
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Ganti Password");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        final Drawable upArrow = ContextCompat.getDrawable(this, R.drawable.abc_ic_ab_back_material);
+        upArrow.setColorFilter(ContextCompat.getColor(this, R.color.black), PorterDuff.Mode.SRC_ATOP);
+        getSupportActionBar().setHomeAsUpIndicator(upArrow);
+
+        changedPassword();
+    }
+
+    private void changedPassword() {
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(ChangePasswordActivity.this, "Changed Password Success", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == android.R.id.home){
+            onBackPressed();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
